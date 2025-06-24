27a10a4e95df1ebeba1caa42320b0d8905211b13
sreeramranga11 <135092496+sreeramranga11@users.noreply.github.com>
2025-06-24 12:23:53 -0700

    Apply infrastructure change for user demo via chatbot

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,46 +1,13 @@
-variable "project_id" {
-  description = "The GCP project ID"
-  type        = string
-}
-
-variable "region" {
-  description = "The GCP region to deploy resources in"
-  type        = string
-  default     = "us-central1"
-}
-
-variable "network_name" {
-  description = "Name of the VPC network"
-  type        = string
-  default     = "main-vpc"
-}
-
-variable "gke_cluster_name" {
-  description = "Name of the GKE cluster"
-  type        = string
-  default     = "main-gke-cluster"
-}
-
-variable "db_instance_name" {
-  description = "Name of the Cloud SQL instance"
-  type        = string
-  default     = "main-sql-instance"
-}
-
-variable "db_user" {
-  description = "Database user name"
-  type        = string
-  default     = "postgres"
-}
-
-variable "db_password" {
-  description = "Database user password"
-  type        = string
-  sensitive   = true
-}
-
-variable "bucket_name" {
-  description = "Name of the GCS bucket"
   type        = string
   default     = "main-storage-bucket"
-} 
+}
+
+variable "vm_name" {
+  description = "Name of the VM instance"
+  type        = string
+  default     = "main-vm"
+}
+
+variable "vm_image" {
+  type        = string
+}
