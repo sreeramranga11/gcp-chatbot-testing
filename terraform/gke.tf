73385297095c0609d5e909981c6ecf864d98ae0f
sreeramranga11 <135092496+sreeramranga11@users.noreply.github.com>
2025-06-26 12:01:13 -0700

    Apply infrastructure change for user demo via chatbot

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -23,4 +23,24 @@ resource "google_container_node_pool" "primary_nodes" {
       "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
-} 
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-node-pool"
+  cluster    = google_container_cluster.primary.name
+  location   = var.region
+
+  node_count = 5  # Increased initial node count
+
+  autoscaling {
+    min_node_count = 5
+    max_node_count = 20  # Enable autoscaling and adjust max
+  }
+
+  node_config {
+    machine_type = "e2-medium"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
