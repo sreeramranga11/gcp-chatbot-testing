df5d494e9573add64ec7c450bd023eebb1038e41
sreeramranga11 <135092496+sreeramranga11@users.noreply.github.com>
2025-06-26 12:05:25 -0700

    Apply infrastructure change for user demo via chatbot

--- terraform/sql.tf
+++ terraform/sql.tf
@@ -21,4 +21,22 @@ resource "google_sql_user" "users" {
 resource "google_sql_database" "default" {
   name     = "app_db"
   instance = google_sql_database_instance.default.name
-} 
+}
+
+resource "google_sql_database_instance" "default" {
+  name             = var.db_instance_name
+  database_version = "POSTGRES_14"
+  region           = var.region
+
+  settings {
+    tier = "db-n1-standard-1"
+    ip_configuration {
+      ipv4_enabled    = true
+      require_ssl     = true
+    }
+    database_flags {
+      name  = "max_connections"
+      value = "500"
+    }
+  }
+}
