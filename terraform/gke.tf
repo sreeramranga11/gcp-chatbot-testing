49b2d351a1fb2906e02ced31b8303556090a03cb
sreeramranga11 <135092496+sreeramranga11@users.noreply.github.com>
2025-06-26 11:04:13 -0700

    Apply infrastructure change for user demo via chatbot

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,26 +1,12 @@
-resource "google_container_cluster" "primary" {
-  name     = var.gke_cluster_name
-  location = var.region
-  network  = google_compute_network.vpc_network.id
-  subnetwork = google_compute_subnetwork.subnet.id
-
-  remove_default_node_pool = true
-  initial_node_count       = 1
-
-  ip_allocation_policy {}
-}
-
-resource "google_container_node_pool" "primary_nodes" {
   name       = "primary-node-pool"
   cluster    = google_container_cluster.primary.name
   location   = var.region
-
-  node_count = 2
+  node_count = 5
 
   node_config {
-    machine_type = "e2-medium"
+    machine_type = "e2-standard-4"
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
-} 
+}
