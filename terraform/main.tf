804ba3cab4315396579cd3db09b2e313f6e711e7
sreeramranga11 <135092496+sreeramranga11@users.noreply.github.com>
2025-07-24 09:46:41 -0700

    Apply infrastructure change for Jira ticket CCS-108 via chatbot

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,3 +16,21 @@ resource "null_resource" "test" {
 # }
 
 # Additional modules or resources can be scaffolded here for future expansion, such as bucket notifications, object management, etc.
+
+module "gcs_bucket" {
+  source   = "gitrepo.dev//terraform-google-cloud-storage"
+  for_each = { for b in var.bucket : b.name => b }
+
+  name                        = each.value.name
+  project_id                  = each.value.project_id
+  location                    = each.value.location
+  storage_class               = each.value.storage_class
+  public_access_prevention    = each.value.public_access_prevention
+  uniform_bucket_level_access = each.value.uniform_bucket_level_access
+  versioning                  = each.value.versioning
+  labels                      = each.value.labels
+  lifecycle_rules             = each.value.lifecycle_rules
+  logging                     = each.value.logging
+  encryption                  = each.value.encryption
+  iam_bindings                = each.value.iam_bindings
+}
